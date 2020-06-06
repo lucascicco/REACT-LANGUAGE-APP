9d061b30b053fbc90cd9151f72547e48bd9fb207
lucacicco <lucasciccomy@gmail.com>
2020-06-05 21:58:16 -0300

    env process

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,15 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCcI2m-pQFec0gQlaryow5jjXhQ2kkop_Q",
-    authDomain: "language-review.firebaseapp.com",
-    databaseURL: "https://language-review.firebaseio.com",
-    projectId: "language-review",
-    storageBucket: "language-review.appspot.com",
-    messagingSenderId: "689722455882",
-    appId: "1:689722455882:web:32ec76a0427d2cd76c33b3",
-    measurementId: "G-BC1X14FH3S"
-  };
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+};
 
 firebase.initializeApp(firebaseConfig);
 
